1e6f837d00b13c5680b6d96a33820fd9d4b9c5de
Cesar Ramirez <LS19070100@jerez.tecnm.mx>
2020-10-14 22:52:09 -0500

    Segun yo solo le iba a dar una checada a el codigo y termine casi terminando el programa, ya solo falta la parte donde se le quita la cima a la parte estatica al momento de rentar una pelicula

--- src/PruebaPilas.java
+++ src/PruebaPilas.java
@@ -1,7 +1,39 @@
+import java.awt.Menu;
 import java.util.Scanner;
 import java.util.Stack;
+interface correccion{
+	Scanner entrada = new Scanner(System.in);
+	public static int validacion() {
+		int r = 0;
+		boolean e = false;
+		
+		do {
+			try {
+				
+				
+				r = entrada.nextInt();
+				
+				
+			} catch (java.util.InputMismatchException x) {
+				System.out.println("Ups! el dato que intentas ingresar no es valido");
+				entrada.nextLine();
+				e=true;
+			}
+			if (r>0) {
+				e=false;
+			}else {
+				System.out.println("Ingresa porfavor solo numeros mayores a 0");
+				e=true;
+			}
+		}while(e);
+		return r;
+	}
+}
 
-interface RentaPeliculas{
+
+
+
+interface RentaPeliculas extends correccion  {
 	public default void  rentarPelicula() {
 		//Es como quitar
 	}
@@ -11,6 +43,21 @@ interface RentaPeliculas{
 	public default void cargarBD() {
 		
 	}
+	public default int menu() {
+		System.out.println("====MENU de renta de peliculas====");
+		System.out.println("Digite 1, para gargar el catalogo de peliculas (5 peliculas)");
+		System.out.println("Digite 2 para devolver/añadir una pelicula");
+		System.out.println("Digite 3 para rentar/eliminar una pelicual de la cima");
+		System.out.println("Digite 4 para mostrar la Base de Datos de las peliculas");
+		System.out.println("Digite 5 para ***SALIR***");
+		int e = correccion.validacion();
+		return e;
+	}
+	public default void mostrarCatalogo() {
+		
+	}
+	
+	
 }
 
 
@@ -56,7 +103,7 @@ class ImplementacionPilaEstatica implements RentaPeliculas{
 	
 	
 }
-class ImplementacionPilaDinamica implements RentaPeliculas{
+class ImplementacionPilaDinamica implements RentaPeliculas , correccion{
 	Stack<Pelicula> pelis = new Stack<Pelicula>();
 	Scanner entrada = new Scanner (System.in);
 	
@@ -90,9 +137,8 @@ public String toString() {
 		
 	}
 	
-	public void cargarBD() {
-		
-				for (int i = 0; i < 5; i++) {
+	public void cargarBD() {		
+			for (int i = 0; i < 5; i++) {
 			devolverPelicula();
 		}
 		
@@ -103,14 +149,57 @@ public String toString() {
 	}
 	
 	
+	
+	
+	
 }
 
 
 public class PruebaPilas {
 
 	public static void main(String[] args) {
-		ImplementacionPilaDinamica ipd = new ImplementacionPilaDinamica();
+		int x ;
 		
+		do {
+			
+		
+		System.out.println("=== MENU de tipo de pilas ===");		
+		System.out.println("Digite 1 para usar Pila ESTATICA");
+		System.out.println("Digite 2 para usar Pila DINAMICA");
+		x = correccion.validacion();
+		switch (x) {
+		case 1:
+			System.out.println("Ingrese el tamaño de la pila");
+			int b = correccion.validacion();
+			ImplementacionPilaEstatica ipe = new ImplementacionPilaEstatica(b);
+			break;
+		case 2:
+			ImplementacionPilaDinamica ipd = new ImplementacionPilaDinamica();
+			int op;
+			do {
+				op = ipd.menu();
+				switch (op) {
+				case 1:ipd.cargarBD();break;
+				case 2: ipd.devolverPelicula();break;
+				case 3: ipd.rentarPelicula();break;
+				case 4: ipd.mostrarCatalogo();break;
+				case 5:System.out.println("Ha salido del menu de la pila Dinamica"); break;
+				
+				}
+			}while(op!=5);;
+		
+		}
+		
+		
+		}while(x!=3);
+		
+		
+		
+		
+		
+		
+		
+		//		
 		
 	}
 
